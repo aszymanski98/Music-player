b2aaf2d2acee048d3555c24f71604a22254d429c
Adam Szymański <adam.szymanski@protonmail.com>
2022-05-21 19:18:41 +0200

    splitted into components

--- src/main.js
+++ src/main.js
@@ -7,4 +7,9 @@ import store from './store';
 import './assets/tailwind.css';
 import './assets/main.css';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.mount('#app');
