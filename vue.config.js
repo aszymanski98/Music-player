a6b4d09f5d2688bda87ca5c7c3f36e5cb968578a
Adam Szymański <adam.szymanski@protonmail.com>
2022-06-19 14:28:44 +0200

    changed publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/Music-player/' : '/',
   pwa: {
     name: 'Music App',
     themeColor: '#ff5e3a',
