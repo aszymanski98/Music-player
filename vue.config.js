a72adeb7cfa37cca99a7768c2829d901cd9d4aff
Adam Szymański <adam.szymanski@protonmail.com>
2022-06-21 21:11:35 +0200

    changes before deploy to vercel

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/Music-player/' : '/',
   pwa: {
     name: 'Music App',
     themeColor: '#ff5e3a',
